73f8c023c53e1165d8f1cd4bfd27c61175f2a61f
The Joker <joshua.thomason@avx.com>
2018-03-13 23:01:17 -0400

    first

--- server.py
+++ server.py
@@ -1,4 +1,20 @@
+# TODO server listens on localhost:port
+# TODO client handler, accept connection and receive data
+# TODO client sender, send buffer over socket connection
+# TODO run command, send commands to CLIENT over socket NOT SERVER
+
 import socket, sys
 
-def server
+def client_handler(client_socket):
 
+
+def client_sender(buffer):
+
+
+def server_loop():
+
+
+def run_command(command):
+
+
+def main():
